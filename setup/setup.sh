f2d2dd31821ba741b71eba05c681a499796c5c81
Aleks <crni.aki@gmail.com>
2025-06-12 07:53:29 +0200

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,3 +11,6 @@ pip install --no-cache-dir -r ./requirements.txt
 
 # Install Torch, Torchvision, and Torchaudio for CUDA 12.2
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu122/torch_stable.html
+
+
+python3 -v
