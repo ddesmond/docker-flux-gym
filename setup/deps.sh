d6440e00f2f1e42aafc45a3ce807c24a4a6dc751
Aleks <crni.aki@gmail.com>
2025-06-14 07:17:29 +0200

    update

--- setup/deps.sh
+++ setup/deps.sh
@@ -43,6 +43,6 @@ apt-get install -y --no-install-recommends 	\
   uuid-dev \
   build-essential libssl-dev zlib1g-dev libbz2-dev \
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-  xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+  xz-utils tk-dev libffi-dev liblzma-dev
 
 updatedb
