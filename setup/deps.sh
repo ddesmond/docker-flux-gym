9d1c1c5a5de2a9ffcb9bdfda7c8e68d76f0084de
Aleks <crni.aki@gmail.com>
2025-06-14 07:20:43 +0200

    update

--- setup/deps.sh
+++ setup/deps.sh
@@ -43,6 +43,14 @@ apt-get install -y --no-install-recommends 	\
   uuid-dev \
   build-essential libssl-dev zlib1g-dev libbz2-dev \
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-  xz-utils tk-dev libffi-dev liblzma-dev
+  xz-utils tk-dev libffi-dev liblzma-dev \
+
 
 updatedb
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
